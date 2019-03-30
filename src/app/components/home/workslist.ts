c7d12b51011a2f6bdccd016ecb49b750374ee8d5
DaniAntu <d.asenjo.c@gmail.com>
2019-03-30 15:22:25 -0300

    Listado de trabajos para Home

---
+++ src/app/components/home/workslist.ts
@@ -0,0 +1,28 @@
+import { Works } from '../../../models/works';
+
+export const WORKS: Works[] = [
+  {
+    nombre: 'Elsa Benitez',
+    ocupacion: 'Gasfiter',
+    descripcion: 'Se requiere contratar Gasfiter para reparar mi baño',
+    telefono: 56941234567,
+    ubicacion: 'Santiago',
+    estado: 'publicado'
+  },
+  {
+    nombre: 'Jacinta Avendaño',
+    ocupacion: 'Diseñadora Gráfica',
+    descripcion: 'Necesito Diseñadora, para tener tarjetas de presentación de mi emprendimiento',
+    telefono: 56987654321,
+    ubicacion: 'Concepción',
+    estado: 'publicado'
+  },
+  {
+    nombre: 'Paloma Castillo',
+    ocupacion: 'Maquilladora',
+    descripcion: 'Maquilladora Profesional, para mi matrimonio',
+    telefono: 56943452341,
+    ubicacion: 'Santiago',
+    estado: 'publicado'
+  }
+];
