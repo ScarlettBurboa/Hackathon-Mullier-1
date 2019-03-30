5e0b4b913925cc5dc3724dce17cdce52d2858d33
DaniAntu <d.asenjo.c@gmail.com>
2019-03-30 09:32:04 -0300

    Inicializa proyecto en Angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/users/login/login.component';
+import { RegisterComponent } from './components/users/register/register.component';
+import { ProfileComponent } from './components/users/profile/profile.component';
+import { HomeComponent } from './components/home/home.component';
+import { OpportunitiesComponent } from './components/opportunities/opportunities.component';
+import { JobsComponent } from './components/jobs/jobs.component';
+import { Page404Component } from './components/page404/page404.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    HomeComponent,
+    OpportunitiesComponent,
+    JobsComponent,
+    Page404Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAhHkJgYeG9m6Q2pBJR-p6lw5p7DzXi0uI",
+    authDomain: "mullier.firebaseapp.com",
+    databaseURL: "https://mullier.firebaseio.com",
+    projectId: "mullier",
+    storageBucket: "mullier.appspot.com",
+    messagingSenderId: "1091830162703"
+  }
+};
